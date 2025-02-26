935089b08cbf0006b48b2bc01affb76589e4105c
Robert Lee <rlee4408@gmail.com>
2025-02-26 19:41:55 +0800

    Add codes

--- week8/agents/ensemble_agent.py
+++ week8/agents/ensemble_agent.py
@@ -4,7 +4,7 @@ import joblib
 
 from agents.agent import Agent
 from agents.specialist_agent import SpecialistAgent
-from agents.frontier_agent import FrontierAgent
+# from agents.frontier_agent import FrontierAgent
 from agents.random_forest_agent import RandomForestAgent
 
 class EnsembleAgent(Agent):
@@ -19,7 +19,7 @@ class EnsembleAgent(Agent):
         """
         self.log("Initializing Ensemble Agent")
         self.specialist = SpecialistAgent()
-        self.frontier = FrontierAgent(collection)
+        # self.frontier = FrontierAgent(collection)
         self.random_forest = RandomForestAgent()
         self.model = joblib.load('ensemble_model.pkl')
         self.log("Ensemble Agent is ready")
@@ -34,14 +34,16 @@ class EnsembleAgent(Agent):
         """
         self.log("Running Ensemble Agent - collaborating with specialist, frontier and random forest agents")
         specialist = self.specialist.price(description)
-        frontier = self.frontier.price(description)
+        # frontier = self.frontier.price(description)
         random_forest = self.random_forest.price(description)
         X = pd.DataFrame({
             'Specialist': [specialist],
-            'Frontier': [frontier],
+            # 'Frontier': [frontier],
             'RandomForest': [random_forest],
-            'Min': [min(specialist, frontier, random_forest)],
-            'Max': [max(specialist, frontier, random_forest)],
+            # 'Min': [min(specialist, frontier, random_forest)],
+            # 'Max': [max(specialist, frontier, random_forest)],
+            'Min': [min(specialist, random_forest)],
+            'Max': [max(specialist, random_forest)],
         })
         y = self.model.predict(X)[0]
         self.log(f"Ensemble Agent complete - returning ${y:.2f}")
